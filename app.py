559d4ea1bfba682874a98fe99266a31bd505073b
xuzhiyuan <xuzhiyuan@nibirutech.com>
2020-04-13 10:27:38 +0800

    add master search view

--- app.py
+++ app.py
@@ -1,23 +1,33 @@
-from flask import Flask, render_template, redirect, url_for
+from flask import Flask, render_template, redirect, url_for, request
 from flask_sqlalchemy import SQLAlchemy
 from wtforms import StringField, IntegerField
 from wtforms.validators import DataRequired
 from flask_wtf import FlaskForm
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://qianyang:@localhost/pokemon'
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://qianyang:@localhost/pokemon'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:tap4fun@172.20.90.11/test'
 app.config['SECRET_KEY'] = 'secret'
 app.config['DEBUG'] = True
 db = SQLAlchemy(app)
 
+@app.before_first_request
+def create_db():
+    # Recreate database each time for demo
+    db.create_all()
+
+def serialize_list(model_list):
+    return [m.as_dict() for m in model_list]
 
 class Pokemon(db.Model):
     __tablename__ = 'pokemon'
-    id = db.Column(db.Integer, primary_key=True)
+    id = db.Column(db.Integer, primary_key=True,autoincrement=True)
     name = db.Column(db.String(100), unique=True)
     type = db.Column(db.String(100))
     attack = db.Column(db.Integer)
 
+    def as_dict(self):
+        return {c.name: getattr(self, c.name) for c in self.__table__.columns}
 
 class NewPokemonForm(FlaskForm):
     name = StringField("Name", validators=[DataRequired()])
@@ -28,6 +38,15 @@ class NewPokemonForm(FlaskForm):
 class DeletePokemonForm(FlaskForm):
     id = IntegerField()
 
+@app.route("/search", methods=['GET', 'POST'])
+def master():
+    if request.method == 'GET':
+        return render_template('master.html')
+    if request.method == 'POST':
+        name = request.form.get('name')
+        results = Pokemon.query.filter(Pokemon.name.like('%{}%'.format(name))).all()
+        return render_template('master.html',results=results)
+        
 
 @app.route("/")
 def index(form=None):
